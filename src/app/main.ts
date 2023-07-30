c40e155d323408077744a9803532e67e6302e92c
J. Alexis TH <colores.2808.8000@gmail.com>
2023-07-29 22:44:29 -0600

    Creando un CRUD

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,32 @@
-import { addProduct } from "./products/products.service";
+import { faker } from '@faker-js/faker';
+import { addProduct, products } from "./products/products.service";
 
-addProduct({
-    id: '10',
-    title: "Producto #1",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    stock: 80,
-    category: {
-        id: '45',
-        name: "Computacion",
-        createdAt: new Date(),
-        updatedAt: new Date()
-    }
-})
+
+for (let index = 0; index < 50; index++) {
+
+    addProduct({
+        id: faker.string.uuid(),
+        description: faker.commerce.productDescription(),
+        image: faker.image.url(),
+        color: faker.color.human(),
+        size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
+        price: parseInt(faker.commerce.price()),
+        isNew: faker.datatype.boolean(),
+        tags: faker.helpers.arrayElements(['Tag1', 'Tag2', 'Tag3']),
+        title: faker.commerce.productName(),
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent(),
+        stock: faker.helpers.rangeToNumber({min: 10, max: 100}),
+        category: {
+            id: faker.string.uuid(),
+            name: faker.commerce.department(),
+            createdAt: faker.date.recent(),
+            updatedAt: faker.date.recent()
+        }
+    })  
+}
+
+
+console.log(products);
+
+
